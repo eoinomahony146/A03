7dc3241fa257fd42490b00cba0fc79c0cb0a0321
Eoin <119492694@umail.ucc.ie>
2021-12-03 14:17:42 +0000

    Fixed if statements

--- src/main/java/a03/a03/LoginController.java
+++ src/main/java/a03/a03/LoginController.java
@@ -40,28 +40,47 @@ public class LoginController {
 
     public void loginValidation() throws IOException {
         Main m = new Main();
-        if (!email.getText().matches("^^[\\\\w!#$%&'*+/=?`{|}~^-]+(?:\\\\.[\\\\w!#$%&'*+/=?`{|}~^-]+" +
-                ")*@(?:[a-zA-Z0-9-]+\\\\.)+[\\\\a-zA-Z]{2,6}") &&
-                password.getText().matches("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])$")
-                && password.getText().length() >= 7)
-                login_message.setText("Success");
+        if (password.getText().isBlank() || email.getText().isBlank())
+            login_message.setText("Please enter your email and password");
         else {
-            if (email.getText().isBlank() && password.getText().isBlank())
-                login_message.setText("Please enter your email and password");
+            if (!email.getText().matches("[a-zA-Z0-9-+=$%^&*_]+@[a-zA-Z0-9-+=$%^&*_]+\\.com"))
+                login_message.setText("Email format is not correct");
             else {
-                if (!email.getText().matches("^[\\\\w!#$%&'*+/=?`{|}~^-]+(?:\\\\.[\\\\w!#$%&'*+/=?`{|}~^-]+" +
-                        ")*@(?:[a-zA-Z0-9-]+\\\\.)+[\\\\a-zA-Z]{2,6}") && !password.getText().isBlank())
-                    login_message.setText("Please enter your email");
-                else if (password.getText().isBlank() && email.getText().matches("^[A-Za-z0-9+_.-]+@(.+)$"))
-                    login_message.setText("Please enter your password");
+                if (password.getText().length() <= 7)
+                    login_message.setText("Password must be at least 7 characters");
                 else {
-                    if (password.getText().length() < 7)
-                        login_message.setText("Password must be at least 6 characters");
-                        if (!password.getText().matches("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])$"))
-                            login_message.setText("Please enter password with at least 1 digit, 1 upper and lower case" +
-                                    "and 1 special character");
+                    if (!password.getText().matches("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+-._=])(?=\\S+$).{7,}$"))
+                        login_message.setText("Please enter password with at least 1 digit, 1 upper and lower case " +
+                                "and 1 special character");
+                    else {
+                        login_message.setText("Success!");
+                    }
                 }
             }
         }
+//        if (!email.getText().matches("^[\\\\w!#$%&'*+/=?`{|}~^-]+(?:\\\\.[\\\\w!#$%&'*+/=?`{|}~^-]+" +
+//                ")*@(?:[a-zA-Z0-9-]+\\\\.)+[\\\\a-zA-Z]{2,6}") &&
+//                password.getText().matches("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])$")
+//                && password.getText().length() >= 7)
+//                login_message.setText("Success");
+//        else {
+//            if (email.getText().isBlank() && password.getText().isBlank())
+//                login_message.setText("Please enter your email and password");
+//            else {
+//                if (!email.getText().matches("^[\\\\w!#$%&'*+/=?`{|}~^-]+(?:\\\\.[\\\\w!#$%&'*+/=?`{|}~^-]+" +
+//                        ")*@(?:[a-zA-Z0-9-]+\\\\.)+[\\\\a-zA-Z]{2,6}") && password.getText().matches("^(?=.*[0-9])(?=.*[a-z])" +
+//                        "(?=.*[A-Z])(?=.*[@#$%^&+=])$"))
+//                    login_message.setText("Please enter your email");
+//                else if (password.getText().isBlank() && email.getText().matches("^[A-Za-z0-9+_.-]+@(.+)$"))
+//                    login_message.setText("Please enter your password");
+//                else {
+//                    if (password.getText().length() < 7)
+//                        login_message.setText("Password must be at least 6 characters");
+//                        if (!password.getText().matches("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])$"))
+//                            login_message.setText("Please enter password with at least 1 digit, 1 upper and lower case" +
+//                                    "and 1 special character");
+//                }
+//            }
+//        }
     }
 }
